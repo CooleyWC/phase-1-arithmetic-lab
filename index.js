55a1e58e2202aecf7d943b403c006cb66ef96c9a
Will Cooley <william.cooley@gmail.coom>
2023-07-18 22:25:43 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 7.75;
+const num2 = 8;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() +1);
+
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+const max = Math.max(2, 4, 7, 5, 20);
